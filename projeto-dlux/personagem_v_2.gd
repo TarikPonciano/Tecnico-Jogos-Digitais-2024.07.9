551095971df2f8616d54cb8b6baf195f1bf871ca
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-08-14 14:40:01 -0300

    Pulo Duplo

---
+++ projeto-dlux/personagem_v_2.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var pulo_duplo = true #Se for true, pode pular uma segunda vez, se não não pode
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		pulo_duplo = true
+		
+	elif Input.is_action_just_pressed("ui_accept") and pulo_duplo:
+		velocity.y = JUMP_VELOCITY
+		pulo_duplo = false
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
