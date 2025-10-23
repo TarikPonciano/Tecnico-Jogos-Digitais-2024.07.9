5930ccc3fe586ca96de5a23798bded15109ff8d0
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-10-23 15:03:16 -0300

    Mecanica porta

--- projeto-dlux/fase_3.gd
+++ projeto-dlux/fase_3.gd
@@ -8,5 +8,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if $"Personagem V 2".cerejas >= 5:
-		get_tree().change_scene_to_file("res://fase_2.tscn")
+	pass
+	#if $"Personagem V 2".cerejas >= 5:
+		#get_tree().change_scene_to_file("res://fase_2.tscn")
